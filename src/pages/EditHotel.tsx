fddd6c7d82d27639d37108ff07307e431f3c42f6
Giorgi Patsia <gio.patsia@gmail.com>
2024-04-24 16:40:05 +0200

    added toast for update hotel

--- src/pages/EditHotel.tsx
+++ src/pages/EditHotel.tsx
@@ -2,8 +2,10 @@ import { useParams } from "react-router-dom";
 import * as apiClient from "../api/api-client";
 import { useMutation, useQuery } from "react-query";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../context/AppContext";
 const EditHotel = () => {
   const { hotelId } = useParams();
+  const { showToast } = useAppContext();
   const { data: hotel } = useQuery(
     "fetchMyHotelById",
     () => apiClient.fetchMyHotelById(hotelId || ""),
@@ -13,8 +15,12 @@ const EditHotel = () => {
   );
 
   const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
-    onSuccess: () => {},
-    onError: () => {},
+    onSuccess: () => {
+      showToast({ message: "Hotel Updated!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Hotel Not Updated!", type: "ERROR" });
+    },
   });
 
   const handleSave = (hotelFormData: FormData) => {
